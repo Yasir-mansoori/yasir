65695e1a62490b81558327c564c252c295064a79
Yasir Raza <128062142+Yasir-mansoori@users.noreply.github.com>
2023-05-24 15:49:55 +0500

    sign up

---
+++ app.js
@@ -0,0 +1,23 @@
+var first = document.getElementById("firstname");
+var sur = document.getElementById("surname");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var date = document.getElementById("date");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
+var male = document.getElementById("Male");
+var female = document.getElementById("Female");
+var custom = document.getElementById("custom");
+
+function register() {
+    console.log(firstname.value);
+    console.log(surname.value);
+    console.log(email.value);
+    console.log(password.value);
+    console.log(date.value);
+    console.log(month.value);
+    console.log(year.value);
+    console.log(male.value);
+    console.log(female.value);
+    console.log(custom.value);
+}
